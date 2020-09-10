b6f97051550620b6ba3904745875caffbeef48d9
MortenGitKonto <morten.jogi@gmail.com>
2020-09-10 16:14:22 +0300

    update website

--- src/main/java/ee/bcs/valiit/controller/SpringSecurityConfiguration.java
+++ src/main/java/ee/bcs/valiit/controller/SpringSecurityConfiguration.java
@@ -14,6 +14,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure (HttpSecurity http) throws Exception{
         http.authorizeRequests()
+                .antMatchers("/createClient.html").permitAll()
                 .anyRequest().authenticated()//permitAll()
                 .and()
                 .formLogin()
